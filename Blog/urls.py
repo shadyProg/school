316c0461ee9786483055fc27c760a26c666ee6d0
shady <shady91c@gmail.com>
2026-09-02 19:22:52 +0300

    feat: add blog list and detail views with reusable context

--- Blog/urls.py
+++ Blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 app_name = 'Blog'
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='blog_list'),
-    path('details/<int:pk>/', views.BlogDetailView.as_view(), name='blog_single'),
+    path('details/<int:pk>/', views.BlogDetailView.as_view(), name='blog_detail'),
     ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,51 +1,77 @@
+from multiprocessing import context
+import re
+
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from Blog.froms import CRUD_BlogForm
+from siteui.views import get_section_context
 from .models import Blog
 
 
 # Function-based view
-
+'''
 def blog_home(request):
     blogs = Blog.objects.all()
-    return render(request, 'Blog/blog_list.html', {'blogs': blogs})
-
+    return render(request, 'blog.html', {'blogs': blogs})
+'''
 
 # Class-based views
 
 class BlogListView(ListView):
     model = Blog
-    template_name = 'Blog/blog_list.html'
+    template_name = 'blog.html'
     context_object_name = 'blogs'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(get_section_context('Blogs', 'Blog'))
+        return context
+        """
+        def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+
+            context.update(
+                get_section_context('Blogs', 'Blog')
+            )
+
+            return context
+        """
     # ordering = ['-created_at'] don`t need to add ordering here because we already added it in the model Meta class.
 
 
 class BlogDetailView(DetailView):
     model = Blog
-    template_name = 'Blog/blog_single.html'
+    template_name = 'blog_single.html'
     context_object_name = 'blog'
 
 
 class BlogCreateView(CreateView):
     model = Blog
     form_class = CRUD_BlogForm
-    template_name = 'Blog/blog_form.html'
-    fields = ['title', 'category', 'description', 'image', 'user']
+    template_name = 'blog_form.html'
+    #fields = ['title', 'category', 'description', 'image', 'user']
     success_url = reverse_lazy('blog:blog_list')
-
+    #from_validation = True  # This is a custom attribute to indicate that form validation is enabled.
+    def form_valid(self, form):
+        # Custom form validation logic can be added here if needed.
+        form.instance.user = self.request.user
+        return super(BlogCreateView, self).form_valid(form)
 
 class BlogUpdateView(UpdateView):
     model = Blog
-    template_name = 'Blog/blog_form.html'
-    fields = ['title', 'category', 'description', 'image', 'user']
+    form_class = CRUD_BlogForm
+    template_name = 'blog_form.html'
     success_url = reverse_lazy('blog:blog_list')
+    def form_valid(self, form):
+        
+        form.instance.user = self.request.user
+        return super(BlogCreateView, self).form_valid(form)
 
 
 class BlogDeleteView(DeleteView):
     model = Blog
-    template_name = 'Blog/blog_confirm_delete.html'
-    context_object_name = 'blog'
+    template_name = 'blog_confirm_delete.html'
     success_url = reverse_lazy('blog:blog_list')
 
 
